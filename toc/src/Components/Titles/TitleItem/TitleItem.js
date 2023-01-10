7999bf70f7c7709a088fdbb1828c93d6ad7221ed
dilnabz <artikovadilnoza19@gmail.com>
2023-01-10 14:59:08 +0300

    fixing toc logic

--- toc/src/Components/Titles/TitleItem/TitleItem.js
+++ toc/src/Components/Titles/TitleItem/TitleItem.js
@@ -1,12 +1,14 @@
 import React, {useState} from "react";
+import {Titles} from '../Titles';
 
 export function TitleItem({entities, id}) {
+    const [isOpen, setIsOpen] = useState(false)
     const { title, pages } = entities.pages[id];
     return (
         <li> 
             {pages !== undefined && (
                 <button onClick={() => setIsOpen(!isOpen)}>
-                    {isOpen ? '↑' : '↑'}
+                    {isOpen ? '↑' : '↓'}
                 </button>
             )}
             {title}
--- toc/src/Components/Titles/Titles.js
+++ toc/src/Components/Titles/Titles.js
@@ -1,26 +1,16 @@
-import React, {useState} from "react";
+import React from "react";
+import { TitleItem } from "./TitleItem/TitleItem";
 
 export function Titles({entities, ids}) {
-    const [isOpen, setIsOpen] = useState(false)
     if (ids === undefined) {
         return null;
     }
     return (
         <ul>
             {ids.map(id => {
-                const { title, pages } = entities.pages[id];
                 return (
                     <li> 
-                        {pages !== undefined && (
-                            <button onClick={() => setIsOpen(!isOpen)}>
-                                {isOpen ? '↑' : '↑'}
-                            </button>
-                        )}
-                        {title}
-                        {isOpen && <Titles
-                            entities={entities}
-                            ids={pages}
-                        />}
+                        <TitleItem entities={entities} id = {id}/>
                     </li>
                 );
             })}
